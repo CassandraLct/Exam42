72d90c4fc342e9581684d3564e620fd37191e24a
Cassandra Lecat <clecat@c3r1p6.42nice.fr>
2022-09-29 16:20:08 +0200

    revision

--- level3/rev_wstr3.c
+++ level3/rev_wstr3.c
@@ -6,14 +6,11 @@
 /*   By: clecat <clecat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 13:57:43 by clecat            #+#    #+#             */
-/*   Updated: 2022/09/29 14:34:50 by clecat           ###   ########.fr       */
+/*   Updated: 2022/09/29 16:18:45 by clecat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 /*write, malloc, free
 Ecrivez un programme qui prend une chaîne de caractères comme paramètre, et
@@ -37,19 +34,32 @@ $*/
 
 int main(int argc, char **argv)
 {
-    int i = 0;
-	int j = 0;
-	char *str;
+	int i = 0;
+	int end;
+	int start;
+	int flag;
 	int x;
     
     if(argc == 2)
     {
-		x = strlen(argv[1]);
-		printf("x = %d\n", x);
         while(argv[1][i] != '\0')
-		{
-			
 			i++;
+		while(i >= 0)
+		{
+			while(argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+				i--;
+			end = i;
+			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+				i--;
+			start = i + 1;
+			flag = start;
+			while(start <= end)
+			{
+				write(1, &argv[1][start], 1);
+				start++;
+			}
+			if(flag != 0)
+				write(1, " ", 1);
 		}
     }
     write(1, "\n", 1);
---
+++ level3/sort_int_tab2.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clecat <clecat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 14:57:30 by clecat            #+#    #+#             */
+/*   Updated: 2022/09/29 15:02:41 by clecat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Write the following function:
+void sort_int_tab(int *tab, unsigned int size);
+Il doit trier (en place) le tableau d'int 'tab', qui contient exactement les
+membres 'size', dans l'ordre croissant.
+Les doubles doivent être préservés.
+L'entrée est toujours cohérente.*/
+
+void    sort_int_tab(int *tab, unsigned int size)
+{
+    
+}
